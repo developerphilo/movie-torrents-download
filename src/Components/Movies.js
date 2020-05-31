a5cdec4f7fb563c9ba004471fdf822980d2c6896
John Philip <developerphilo@gmail.com>
2020-05-31 08:23:36 +0100

    added react-router-dom to the project

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -7,9 +7,10 @@ function Movies( { movies ,  openPopup }) {
     return (
         <div className="movies">
             <div></div>
-            {movies.map(movie =>(
-                <Movie movie={movie} id={movie.id} openPopup={openPopup}/>
-            ))}
+            { movies.map(movie =>(
+                    <Movie movie={movie} id={movie.id} openPopup={openPopup}/>
+                ))
+            }
         </div>
     )
 }
