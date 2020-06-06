3223ff977c184c5dc1aaea616f4864fa4effd454
John Philip <developerphilo@gmail.com>
2020-06-06 09:45:16 +0100

    made some rersponsivenes and smooth scroll

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,7 @@
 import React , { useState, useEffect } from 'react';
 
-import { BrowserRouter as Router } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar , faSearch} from '@fortawesome/free-solid-svg-icons';
+import { faStar  } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar({ handleInput , Search }){
 
